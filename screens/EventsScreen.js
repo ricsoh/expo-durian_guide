c7acaf1a9de87651cf14981760773d95ed5fae48
ricsoh <ricsoh@gmail.com>
2020-12-03 21:54:52 +0800

    add event 3rd screen

--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -18,8 +18,24 @@ function EventsHomeScreen({ navigation }) {
       return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
             <Text>Events Second Page!</Text>
+            <Button
+                onPress={() => navigation.navigate("EventsThird")}
+                title="Third Screen"
+            ></Button>
         </View>
       );
+  }
+
+  function EventsThirdScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>Events Third Page!</Text>
+            <Button
+                onPress={() => navigation.popToTop()}
+                title="Back to top"
+            ></Button>
+        </View>
+    );
   }
 
   const Stack = createStackNavigator();
@@ -29,6 +45,7 @@ function EventsHomeScreen({ navigation }) {
           <Stack.Navigator>
               <Stack.Screen name="EventsHome" component={EventsHomeScreen} />
               <Stack.Screen name="EventsSecond" component={EventsSecondScreen} />
+              <Stack.Screen name="EventsThird" component={EventsThirdScreen} />
           </Stack.Navigator>
       );
   }
